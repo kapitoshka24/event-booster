75a7f98077ded2b0f5dc0a340112e462ccdb216f
Ivan Volobuev <ivannnn2425@gmail.com>
2021-05-10 22:44:29 +0300

    [Search] Add logic for searching events by query

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,11 +1,18 @@
 const API_KEY = 'ARJZnVEmUrYNGar66goghRYaXKGawgDn';
+const options = {
+  searchQuery: "",
+  countryQuery: ""
+}
 const BASE_URL = `https://app.ticketmaster.com/discovery/v2/events.json?size=24&apikey=${API_KEY}`;
 
-export default class EventApiService {
+class EventApiService {
   constructor() {}
   async fetchEvent() {
-    const response = await fetch(`${BASE_URL}`);
+    // &countryCode удалить если фильтрация событий будет без запроса на сервер, а по полученным событиям с сервера после запроса
+    const response = await fetch(`${BASE_URL}&keyword=${options.searchQuery}&countryCode${options.countryQuery}`);
     const eventObj = await response.json();
     return eventObj._embedded.events;
   }
 }
+
+export default {EventApiService, options}
---
+++ src/js/searchQuery.js
@@ -0,0 +1,30 @@
+import api from './apiService';
+import { refs } from './refs';
+import eventRender from './eventRender';
+
+const addSearchQuery = (el) => {
+  if (el.target.name === "search") {
+    searchEventsByQuery(el.target.value)
+  }
+  // для инпута со странами подставить подходящий класс или свой if
+  if (el.target.name === "country") {
+    searchEventsByCountry(el.target.value)
+  } else {
+    return
+  }
+};
+
+const searchEventsByQuery = (value) => {
+  refs.cardContainer.innerHTML = ""
+  api.options.searchQuery = value
+  eventRender()
+}
+
+// функция поиска по странам. заменить на корректную
+// value -- код страны
+const searchEventsByCountry = (value) => {
+  api.options.countryQuery = value
+  eventRender()
+}
+
+export default addSearchQuery
