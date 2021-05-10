e6a20210ebf071190ea157e96e7e84a5c1176349
Ivan Volobuev <ivannnn2425@gmail.com>
2021-05-10 22:51:23 +0300

    [Search] Add logic for searching events by query

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,6 @@ import './apiService';
 import addSearchQuery from './searchQuery';
 import { refs } from './refs';
 import './dropdown-menu';
-import cardContainerMkp from '../templates/card-container.hbs';
 import eventRender from './eventRender';
 import paginationMkp from '../templates/pagination.hbs';
 import debounce from 'lodash.debounce';
