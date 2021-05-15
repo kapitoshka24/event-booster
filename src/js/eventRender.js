ac687cb100625d0f8bf6db7035642bfd6e4e84aa
Nataliia Byshovets <natashabysh11@gmail.com>
2021-05-15 13:32:05 +0300

    [Spinner] Minor fixes

--- src/js/eventRender.js
+++ src/js/eventRender.js
@@ -1,10 +1,11 @@
 import api from './apiService';
 import { refs } from './refs';
-import { showHideSpinner } from './spinner';
+import { toggleSpinner } from './spinner';
 import cardContainerMkp from '../templates/card-container.hbs';
 const eventApiService = new api.EventApiService();
+
 export default async () => {
-  showHideSpinner("remove")
+  toggleSpinner('remove');
   try {
     const result = await eventApiService.fetchEvent();
     refs.cardContainer.insertAdjacentHTML(
@@ -14,6 +15,6 @@ export default async () => {
   } catch (error) {
     console.log(error);
   } finally {
-    showHideSpinner("add")
+    toggleSpinner('add');
   }
 };
--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,39 +1,39 @@
-import {refs} from './refs'
-import storage from './storage'
-import spinner from '../templates/spinner.hbs'
+import { refs } from './refs';
+import storage from './storage';
+import spinner from '../templates/spinner.hbs';
 import eventRender from './eventRender';
 
 const addSpinner = (ref, place) => {
-    refs[ref].insertAdjacentHTML(place, spinner())
-}
+  refs[ref].insertAdjacentHTML(place, spinner());
+};
 
-const toggleSpinner = ({search, addClass = "first-visit"}) => {
-    document.querySelector(search).classList.add(addClass)
-}
+const addClassSpinner = ({ search, addClass = 'first-visit' }) => {
+  document.querySelector(search).classList.add(addClass);
+};
 
-export const showHideSpinner = (param) => {
-    refs.mainPart.querySelector('.spinner').classList[param]('hidden')
-}
+export const toggleSpinner = param => {
+  refs.mainPart.querySelector('.spinner').classList[param]('hidden');
+};
 
 const addFirstVisitStyles = () => {
-    refs.firstVisit.classList.add("first-visit-container")
-    toggleSpinner({search: '.spinner'})
-    toggleSpinner({search: 'div.dot-pulse'})
-    document.querySelector(".first-visit-container").style = "transition: opacity 2s"
-}
+  refs.firstVisit.classList.add('first-visit-container');
+  addClassSpinner({ search: '.spinner' });
+  addClassSpinner({ search: 'div.dot-pulse' });
+  document.querySelector('.first-visit-container').style =
+    'transition: opacity 2s';
+};
 
-if (!storage.getItem("first-visit")) {
-    addSpinner("firstVisit", "afterend")
-    addFirstVisitStyles()
-    setTimeout(() => {
-        addSpinner("mainPart", "beforeend")
-        eventRender()
-        refs.firstVisit.classList.remove("first-visit-container")
-        document.querySelector(".spinner.first-visit").outerHTML = ""
-      storage.setItem("first-visit", true)
-}, 2500)
+if (!storage.getItem('first-visit')) {
+  addSpinner('firstVisit', 'afterend');
+  addFirstVisitStyles();
+  setTimeout(() => {
+    addSpinner('mainPart', 'beforeend');
+    eventRender();
+    refs.firstVisit.classList.remove('first-visit-container');
+    document.querySelector('.spinner.first-visit').outerHTML = '';
+    storage.setItem('first-visit', true);
+  }, 2500);
 } else {
-    addSpinner("mainPart", "beforeend")
-    eventRender()
+  addSpinner('mainPart', 'beforeend');
+  eventRender();
 }
-
