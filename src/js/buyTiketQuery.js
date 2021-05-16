f0f94e796f8778d33f78c2abda9fb7d88b4a5999
teosoph <39537001+teosoph@users.noreply.github.com>
2021-05-17 01:31:06 +0300

    [BuyTicketBtn] Add blur(event) to btnBuyTickets

--- src/js/buyTiketQuery.js
+++ src/js/buyTiketQuery.js
@@ -3,7 +3,6 @@ import { refs } from './refs';
 // open new window dy link from Api.url
 function onClickBtn(urlLink) {
   window.open(urlLink);
-  // window.location.replace(link);
 }
 
 // use addEventListener on event 'click' on btn to by ticket
@@ -11,5 +10,6 @@ refs.backdrop.addEventListener('click', event => {
   if (event.target.matches('.modal-btn-buy-tickets')) {
     let urlLink = event.target.getAttribute('link');
     onClickBtn(urlLink);
+    event.target.blur(event);
   }
 });
