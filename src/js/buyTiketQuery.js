82a45d87a174b15400d59e0d7a7ac1856c76994e
Nataliia Byshovets <natashabysh11@gmail.com>
2021-05-17 20:27:45 +0300

    [Refactoring] Delete comments

--- src/js/buyTiketQuery.js
+++ src/js/buyTiketQuery.js
@@ -1,11 +1,9 @@
 import { refs } from './refs';
 
-// open new window dy link from Api.url
 function onClickBtn(urlLink) {
   window.open(urlLink);
 }
 
-// use addEventListener on event 'click' on btn to by ticket
 refs.backdrop.addEventListener('click', event => {
   if (event.target.matches('.modal-btn-buy-tickets')) {
     let urlLink = event.target.getAttribute('link');
