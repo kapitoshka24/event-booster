80fade0de96a2d135632f9df7f5c34b27f063093
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-05-20 17:27:24 +0300

    [Bad request] Add notices for invalid search queries

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,16 +1,26 @@
 import * as $ from 'jquery';
-import { eventApiService } from './api-service';
+import { options, eventApiService } from './api-service';
 import cardContainerMkp from '../templates/card-container.hbs';
 import { refs } from './refs';
 import { toggleSpinner } from './spinner';
 import { successfullRequest, emptyEvents } from './pnotify';
+import noEventsCountryTpl from '../templates/no-events-country.hbs';
+import noEventsQueryTpl from '../templates/no-events-query.hbs';
+import countries from '../../countries.json';
 
 $(async function () {
   const updatePages = async () => {
     const totalPages = (await eventApiService.fetchData(false)).totalPages;
+    console.log((await eventApiService.fetchData(false)).events);
 
     if (totalPages === 0) {
       refs.cardContainer.innerHTML = '';
+      const country = countries.find(country => country.countryCode === options.countryQuery);
+
+      options.countryQuery
+        ? refs.cardContainer.insertAdjacentHTML('beforeend', noEventsCountryTpl(country))
+        : refs.cardContainer.insertAdjacentHTML('beforeend', noEventsQueryTpl(options.searchQuery));
+    
       refs.paginationList.classList.add('hide-pages');
       emptyEvents();
       toggleSpinner('mainPart', 'add');
